4035955ba2fcce0d5b3bc4d39396d432fa194470
Djalma <djalmirinha@gmail.com>
2019-04-23 16:47:33 -0300

    Dealing with some indentations bullshit

--- classes/imap.py
+++ classes/imap.py
@@ -7,38 +7,38 @@ from classes.email import MailWrapper
 class ImapGmailWrapper:
 
 	def __init__ (self, mail, app_password):
-		self.imap = IMAP4_SSL(host='imap.gmail.com', port=993)
-		self.imap.login(mail, app_password)
+	    self.imap = IMAP4_SSL(host='imap.gmail.com', port=993)
+	    self.imap.login(mail, app_password)
 
 	def select_mail_box(self, mail_box, read_only=True):
-		self.imap.select(mail_box, read_only)
+	    self.imap.select(mail_box, read_only)
 
 	def get_last_email_content(self, search_criteria):
-		last_id = self.get_last_email_id(search_criteria)
-		raw_email = self.fetchs([last_id])
-		mail_parser = MailWrapper(raw_email)
-		clean_mail = mail_parser.get_body_content()
+	    last_id = self.get_last_email_id(search_criteria)
+	    raw_email = self.fetchs([last_id])
+	    mail_parser = MailWrapper(raw_email)
+	    clean_mail = mail_parser.get_body_content()
 
-		return clean_mail
+	    return clean_mail
 
 	def get_last_email_id(self, search_criteria):
-		emails_id = self.search(search_criteria)
+	    emails_id = self.search(search_criteria)
 
-		return int(emails_id[-1])
+	    return int(emails_id[-1])
 
 	def search(self, criteria, charset=None):
 		try:
 			_, mails_id = self.imap.search(charset, '(' + criteria + ')')
 		except:
 			print('bleh')
-
+    
 		return mails_id[0].split()
 
 	def fetchs(self, mail_ids, mail_part='RFC822'):
 		fetched_list = list()
 		for mail_id in mail_ids:
 			fetched = self.single_fetch(mail_id, mail_part)
-			fetched_list.append(fetched)
+		fetched_list.append(fetched)
 
 		if len(fetched_list) == 1:
 			fetched_list = fetched_list[0]
@@ -69,7 +69,7 @@ class ImapGmailWrapper:
 		clean_fetch = clean_fetch[1]
 
 		return clean_fetch
-		 
+
 	@staticmethod
 	def is_full_email(criteria):
-		return criteria == 'RFC822' or criteria == 'BODY[]'
+			return criteria == 'RFC822' or criteria == 'BODY[]'
